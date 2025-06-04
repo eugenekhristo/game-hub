935d174cb6a2b1985e46bdcef7ea772057ff6cbe
Eugene Khristo <marteau9292@gmail.com>
2025-06-04 18:26:48 +0300

    useData generic hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,13 @@
-import useGenres from '@/hooks/useGenres';
+import useData from '@/hooks/useData';
+import { type Genre } from '@/hooks/useGenres';
 
 function GenreList() {
-  const { genres } = useGenres();
+  const { data: genres } = useData<Genre>('/genres');
 
   return (
     <ul>
       {genres.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <li key={genre.name}>{genre.name}</li>
       ))}
     </ul>
   );
